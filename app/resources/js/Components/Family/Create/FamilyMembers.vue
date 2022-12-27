fede312a08a6d62f1003bbf6f32fea04df1ec1ea
eperku <it@eperku.lt>
2022-12-27 19:12:56 +0200

    commit

--- app/resources/js/Components/Family/Create/FamilyMembers.vue
+++ app/resources/js/Components/Family/Create/FamilyMembers.vue
@@ -1,34 +1,9 @@
 <template>
     <VPage>
 
-        <div class="tree w-full h-full flex flex-col justify-center">
-            <div v-for="(layer, layerIndex) in tree"
-                 :key="`layer-${layerIndex}`"
-                 class="tree__layer w-full flex justify-around"
-            >
-                <div v-for="(memberLayer, memberLayerIndex) in layer"
-                     :key="`member-layer-${memberLayerIndex}`"
-                     class="tree__member-layer w-full flex justify-around"
-                >
-                    <Avatar v-for="(member, memberIndex) in memberLayer"
-                            :key="`member-${memberIndex}`"
-                            :subject="member"
-                            class="w-14 h-14"
-                            :class="{
-                                'border-2 solid': layerIndex === currentLayerIndex && memberLayerIndex === currentMemberLayerIndex && memberIndex === currentMemberIndex
-                            }"
-                            @click="select(layerIndex, memberLayerIndex, memberIndex)"
-                    />
-                </div>
-            </div>
-        </div>
-
-        <div class="flex justify-around">
-            <VButton basic shadow @click="addParent">Add parent</VButton>
-            <VButton basic shadow @click="addChildren">Add children</VButton>
-            <VButton basic shadow @click="addWife">Add h/w</VButton>
-            <VButton basic shadow @click="addBrotherOrSister">Add b/s</VButton>
-        </div>
+        <Tree :tree="tree"
+              :members="members"
+        />
 
         <FamilyCreateFooter
             class="px-10"
@@ -43,24 +18,22 @@ import FamilyCreateFooter from '@/Components/Family/Create/Footer';
 import Avatar from '@/Elements/Avatar';
 import {inject, reactive, ref} from 'vue';
 import VButton from '@/Elements/Button';
+import Tree from '@/Elements/Tree/Tree';
 
 export default {
     name: 'FamilyMembers',
     components: {
+        Tree,
         VButton,
         Avatar,
         FamilyCreateFooter,
         VPage
     },
     props: {
-        members: Array,
         progress: Boolean,
     },
     emits: ['back'],
     setup(props, { emit }) {
-        const currentMemberIndex = ref(0);
-        const currentMemberLayerIndex = ref(0);
-        const currentLayerIndex = ref(0);
         const members = reactive([
             { id: 1, name: 'Juozas'},
             { id: 2, name: 'Galina'},
@@ -78,82 +51,53 @@ export default {
             { id: 14, name: 'Agnė'},
         ])
         const tree = reactive([
-            [
-                {
-                    people: [1,2],
-                    children: [12],
-                },
-                {
-                    people: [3,4],
-                    children: [
-                        {
-                            people: [5,12],
-                            children: [
-                                {
-                                    people: [8],
-                                    children: [],
-                                },
-                                {
-                                    people: [9],
-                                    children: [],
-                                }
-                            ],
-                        },{
-                            people: [6,14],
-                            children: [
-                                {
-                                    people: [10],
-                                    children: [],
-                                }, {
-                                    people: [11],
-                                    children: [],
-                                }
-                            ],
-                        },{
-                            people: [7,13],
-                            children: [],
-                        }
-                    ],
-                },
-            ],
+            {
+                people: [1,2],
+                children: [
+                    {
+                        people: [12],
+                        children: [],
+                    }
+                ],
+            },
+            {
+                people: [3,4],
+                children: [
+                    {
+                        people: [5,12],
+                        children: [
+                            {
+                                people: [8],
+                                children: [],
+                            },
+                            {
+                                people: [9],
+                                children: [],
+                            }
+                        ],
+                    },{
+                        people: [6,14],
+                        children: [
+                            {
+                                people: [10],
+                                children: [],
+                            }, {
+                                people: [11],
+                                children: [],
+                            }
+                        ],
+                    },{
+                        people: [7,13],
+                        children: [],
+                    }
+                ],
+            },
         ])
 
         return {
             emit,
             tree,
-            currentMemberIndex,
-            currentMemberLayerIndex,
-            currentLayerIndex,
-            select(layerIndex, memberLayerIndex, memberIndex) {
-                currentLayerIndex.value = layerIndex;
-                currentMemberLayerIndex.value = memberLayerIndex;
-                currentMemberIndex.value = memberIndex;
-            },
-            addParent() {
-                let index = currentLayerIndex.value - 1;
-                if (!Array.isArray(tree[index])) {
-                    tree.unshift([]);
-                    currentLayerIndex.value++;
-                    index++;
-                }
-
-                for (let x = tree[index].length; x < tree[currentLayerIndex.value].length; x++) {
-                    tree[index].push([]);
-                }
-
-                tree[index][currentMemberLayerIndex.value].push({ name: 'Parent' });
-            },
-            addWife() {
-                tree[currentLayerIndex.value][currentMemberLayerIndex.value].push({
-                    name: 'wife'
-                });
-            },
-            addChildren() {
-
-            },
-            addBrotherOrSister() {
-
-            }
+            members,
         };
     },
 }
---
+++ app/resources/js/Elements/Tree/Tree.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-full h-full flex flex-col">
+        <TreeBranch :tree="tree"
+                    :members="members"
+        />
+    </div>
+</template>
+<script>
+import Avatar from '@/Elements/Avatar';
+import TreeBranch from '@/Elements/Tree/TreeBranch';
+export default {
+    name: 'Tree',
+    components: {TreeBranch, Avatar},
+    props: {
+        tree: Object,
+        members: Object,
+    }
+}
+</script>
